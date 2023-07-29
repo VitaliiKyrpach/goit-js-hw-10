87416b0c914a3865724c64b85e74d925cb08f9da
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2023-07-29 18:26:57 +0300

    done

---
+++ src/cat-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const loader = document.querySelector('.loader');
+const BASE_URL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] =
+  'live_nzdubPeONlGVl79RMOfalDti3pTROMTtpLxDRMMWOB63FcpCaothXWQIz0SbX02F';
+
+export function fetchBreeds() {
+  loader.classList.toggle('is-hidden');
+  return axios.get(`${BASE_URL}/breeds`).then(response => {
+    // if (!response.ok) {
+    //   throw new Error(response.statusText);
+    // }
+    return response.data;
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  loader.classList.toggle('is-hidden');
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      return response.data[0];
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const select = document.querySelector('.breed-select');
+const catCard = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const errorMessage = document.querySelector('.error');
+errorMessage.hidden = true;
+select.hidden = true;
+
+fetchBreeds()
+  .then(data => {
+    loader.classList.toggle('is-hidden');
+    select.hidden = false;
+    select.insertAdjacentHTML('beforeend', createSelectMarkup(data));
+    new SlimSelect({
+      select: '.breed-select',
+      settings: {
+        placeholderText: 'Select a cat',
+      },
+      // events: {
+      //   afterChange: newVal => {
+      //     console.log(newVal);
+      //   },
+      // },
+    });
+  })
+  .catch(error => {
+    Notify.failure(errorMessage.textContent);
+    console.log(error);
+  });
+
+function createSelectMarkup(arr) {
+  return (
+    `<option data-placeholder="true"></option>` +
+    arr.map(cat => `<option value="${cat.id}">${cat.name}</option>`).join('')
+  );
+}
+select.addEventListener('change', onChange);
+function onChange() {
+  catCard.classList.toggle('is-hidden');
+  fetchCatByBreed(select.value)
+    .then(catInfo => {
+      loader.classList.toggle('is-hidden');
+      catCard.classList.toggle('is-hidden');
+      catCard.innerHTML = createMarkup(catInfo);
+    })
+    .catch(error => {
+      Notify.failure(errorMessage.textContent);
+      console.log(error);
+    });
+}
+
+function createMarkup(infoArr) {
+  return `<img src="${infoArr.url}" alt="${infoArr.breeds[0].name}" width="500"/><div><h2>${infoArr.breeds[0].name}</h2><p style='width:400px'>${infoArr.breeds[0].description}</p><p><b>Temperament:</b> ${infoArr.breeds[0].temperament}</p></div>`;
+}
